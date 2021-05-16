3ce46614a2d298356620c6edee96ff3c4af66f55
FolksDev <folksdevtv@gmail.com>
2021-05-16 20:40:06 +0200

    initial commit

--- start.bat
+++ start.bat
@@ -1,6 +1,8 @@
+rmdir out /s /q
 mkdir out
-cd src
-javac -d ../out com/imc/paperrockscissors/*.java
-cd ../out
+cd src/main
+javac -d ../../out com/imc/paperrockscissors/*.java
+cd ../../out
 jar cvfe PRSGame.jar com.imc.paperrockscissors.PaperRockScissors com/imc/paperrockscissors/*
 java -jar PRSGame.jar
+cd ../
